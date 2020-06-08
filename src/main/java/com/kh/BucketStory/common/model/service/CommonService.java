9e989c3263bb7bc563ce310c7991f4a7db98b42c
ms <ms@DESKTOP-DMJLA21>
2020-06-08 21:39:06 +0900

    6.8 병민 세번째

--- src/main/java/com/kh/BucketStory/common/model/service/CommonService.java
+++ src/main/java/com/kh/BucketStory/common/model/service/CommonService.java
@@ -1,10 +1,15 @@
 package com.kh.BucketStory.common.model.service;
 
 import com.kh.BucketStory.common.model.vo.Member;
+import com.kh.BucketStory.expert.model.vo.Company;
 
 public interface CommonService {
 
 	Member memberLogin(Member m);
+
+	int insertMember(Member m);
+
+	Company companyLogin(Company c);
 	
 	
 
