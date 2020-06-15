d7bb0d20d53c61d1656761b6dfcfb28d84448d66
LeeSeonJe <tjswp012@gmail.com>
2020-06-15 19:21:34 +0900

    버킷 블로그 작성 commit

--- src/main/java/com/kh/BucketStory/member/model/service/MemberServiceImpl.java
+++ src/main/java/com/kh/BucketStory/member/model/service/MemberServiceImpl.java
@@ -6,6 +6,7 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kh.BucketStory.admin.model.vo.PageInfo;
 import com.kh.BucketStory.bucket.model.vo.BucketList;
 import com.kh.BucketStory.bucket.model.vo.Media;
 import com.kh.BucketStory.member.model.dao.MemberDAO;
@@ -17,10 +18,10 @@ public class MemberServiceImpl implements MemberService {
 
 	@Autowired
 	private MemberDAO mDAO;
-	
+
 	@Autowired
 	private SqlSessionTemplate sqlSession;
-	
+
 	@Override
 	public int bucketInsert(Media m, BucketList bL) {
 		return mDAO.bucketInsert(sqlSession, m, bL);
@@ -35,4 +36,20 @@ public class MemberServiceImpl implements MemberService {
 	public int blogInsert(Board board) {
 		return mDAO.blogInsert(sqlSession, board);
 	}
+
+	@Override
+	public ArrayList<Board> getBoard(Board b) {
+		return mDAO.getBoard(sqlSession, b);
+	}
+
+	@Override
+	public int getListCount() {
+		return mDAO.getListCount(sqlSession);
+	}
+
+	@Override
+	public ArrayList<MemberMyBucketList> myBucketListPage(String userId, PageInfo pi) {
+		return mDAO.myBucketListPage(sqlSession, userId, pi);
+	}
+
 }
