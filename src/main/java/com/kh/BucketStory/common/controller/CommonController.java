b27ebf9c457bcb0847bbd71f67c06afba39ca43b
ms <ms@DESKTOP-DMJLA21>
2020-06-25 21:46:30 +0900

    ㅂㅁ ㅋㅁ

--- src/main/java/com/kh/BucketStory/common/controller/CommonController.java
+++ src/main/java/com/kh/BucketStory/common/controller/CommonController.java
@@ -10,11 +10,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.catalina.User;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.social.MissingAuthorizationException;
+import org.springframework.social.connect.Connection;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.UserOperations;
+import org.springframework.social.facebook.api.impl.FacebookTemplate;
+import org.springframework.social.facebook.connect.FacebookConnectionFactory;
+import org.springframework.social.oauth2.AccessGrant;
+import org.springframework.social.oauth2.GrantType;
+import org.springframework.social.oauth2.OAuth2Operations;
+import org.springframework.social.oauth2.OAuth2Parameters;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,40 +39,103 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.kh.BucketStory.common.model.service.CommonService;
-import com.kh.BucketStory.common.model.service.MailService;
+import com.kh.BucketStory.common.model.vo.Email;
+import com.kh.BucketStory.common.model.vo.EmailSender;
 import com.kh.BucketStory.common.model.vo.Member;
 import com.kh.BucketStory.expert.model.vo.Company;
+import com.sun.javafx.collections.MappingChange.Map;
 
 @SessionAttributes({"loginUser", "loginCompany"})
 @Controller
 public class CommonController {
+
+	@Autowired
+	private EmailSender emailSender;
 	
-	private MailService mailService;
+	@Autowired
+	private Email EEmail;
 	
 	@Autowired // 의존성 주입
 	private CommonService cService;
 	
 	@Autowired
 	private BCryptPasswordEncoder bcryptPasswordEncoder;
-
+	
 	@RequestMapping("login.co")
 	public String LoginView() {
 		return "login";
+		
 	}
 	
+	@Autowired
+    private FacebookConnectionFactory connectionFactory;
+	
+    @Autowired
+    private OAuth2Parameters oAuth2Parameters;
+    
+ // 로그인 테스트
+    @RequestMapping("loginTest.co")
+	public String LoginTest(@ModelAttribute Member m, @ModelAttribute Company c, Model model) {
+    		return "loginTest";
+//    		return "redirect:main.ho?menuNum=1&category=0";
+    	}
+    
 	// 유저 로그인
 	@RequestMapping("memberlogin.co")
+	@ResponseBody
 	public String Login(@ModelAttribute Member m, Model model) {
-		Member loginUser = cService.memberLogin(m); 
-		
-		if(bcryptPasswordEncoder.matches(m.getUserPwd(), loginUser.getUserPwd())) {
-			model.addAttribute("loginUser", loginUser);
-			return "redirect:main.ho?menuNum=1&category=0";
+		Member loginUser = cService.memberLogin(m);
+		String returnString = "";
+		if(loginUser != null) {
+			if(bcryptPasswordEncoder.matches(m.getUserPwd(), loginUser.getUserPwd())) {
+				model.addAttribute("loginUser", loginUser);
+				returnString = "success";
+			} else {
+				System.out.println("로그인 실패");
+				returnString = "fail";
+			}
 		} else {
-			return "redirect:login.co";
+			returnString = "fail";
 		}
+		return returnString;
 	}
 	
+	@RequestMapping("loginSuccess.co")
+	public String LoginSuccess(HttpSession session) {
+		String userId =  ((Member)session.getAttribute("loginUser")).getUserId();
+		if(userId.equals("admin")) {
+			return "redirect:adminwrite.ad";
+		} else {
+			return "loginTest";
+		}
+		
+	}
+	   
+    // 기업 로그인
+  @RequestMapping("companylogin.co") 
+  @ResponseBody
+  public String companyLogin(@ModelAttribute Company c, Model model) {
+	  Company loginUser = cService.companyLogin(c);
+	  String returnString = "";
+	  if(loginUser != null) {
+		  if(bcryptPasswordEncoder.matches(c.getCoPwd(), loginUser.getCoPwd())) {
+			  model.addAttribute("loginCompany", loginUser);
+			  returnString = "success";
+		  } else {
+			  System.out.println("로그인 실패");
+			  returnString = "fail";
+		  }
+	  } else {
+		  returnString = "fail";
+	  }
+	  return returnString;
+  	}
+  
+  	@RequestMapping("loginSuccess2.co")
+  	public String LoginSuccess2() {
+  		return "loginTest";
+	}
+  	
 	// 로그아웃
     @RequestMapping("logout.co")
     public String logout(SessionStatus status) {
@@ -93,21 +168,6 @@ public class CommonController {
     	
     	return returnValue;
     }
-    
-    // 기업 로그인
-	
-  @RequestMapping("companylogin.co") public String companyLogin(@ModelAttribute Company c, Model model) {
-	  Company loginUser = cService.companyLogin(c);
-	  System.out.println(loginUser);
-  
-	  if(bcryptPasswordEncoder.matches(c.getCoPwd(), loginUser.getCoPwd())) {
-		  model.addAttribute("loginCompany", loginUser);
-		  return "redirect:main.ho?menuNum=1&category=0";
-	  } else {
-		  System.out.println("실패");
-		  return "redirect:login.co";
-	  }
-  }
 	 
   	//기업 회원가입
     @RequestMapping("CompanyIn.co")
@@ -186,72 +246,126 @@ public class CommonController {
 		  }
 	  }
     
- 
- // 회원가입 이메일 인증
-    @RequestMapping(value = "sendMail/auth.co", method = RequestMethod.POST, produces = "application/json")
-    @ResponseBody
-    public boolean sendMailAuth(HttpSession session, @RequestParam String email, @RequestParam String busiEmail) {
-        int ran = new Random().nextInt(100000) + 10000; // 10000 ~ 99999
-        String joinCode = String.valueOf(ran);
-        session.setAttribute("joinCode", joinCode);
- 
-        String subject = "회원가입 인증 코드 발급 안내 입니다.";
-        StringBuilder sb = new StringBuilder();
-        sb.append("귀하의 인증 코드는 " + joinCode + " 입니다.");
-        return mailService.send(subject, sb.toString(), "pbmin1993@gmail.com", email, null);
-    }
-
+    // 기업 ID 유효성 체크
+    @RequestMapping("dupid2.co")
+	  public void idDuplicateCheck2(@RequestParam("coId") String id2, HttpServletResponse response) {
+		  
+		  int result = cService.idDuplicateCheck2(id2);
+		 
+		  boolean inUsable = result == 0 ? true : false;
+		  
+		  try {
+			  response.getWriter().print(inUsable);
+		  } catch (IOException e) {
+			  e.printStackTrace();
+			  
+		  }
+	  }
+    
+ // 닉넴 유효성 체크
+    @RequestMapping("nickChk.co")
+	  public void nickNameCheck(@RequestParam("nickName") String nick, HttpServletResponse response) {
+		  
+		  int result = cService.nickNameCheck(nick);
+		 
+		  boolean inUsable = result == 0 ? true : false;
+		  
+		  try {
+			  response.getWriter().print(inUsable);
+		  } catch (IOException e) {
+			  e.printStackTrace();
+			  
+		  }
+	  }
     
  // 아이디 찾기
-//    @RequestMapping(value = "compareEmail.co", method = RequestMethod.POST, produces = "application/json")
-//    public ModelAndView emailCompare(HttpSession session, @RequestParam("email") String email, @RequestParam("email_1") String email_1, @RequestParam("email_2") String email_2, ModelAndView mv) { 
-//		
-//    	int member = cService.compareEmail(email);
-//		
-//		if(email != null) {
-//			email = "redirect:login.co";
-//			
-//		} else {
-//			System.out.println("실패");
-//		}
-//		
-//		return mv;
-//	
-//	}	
-		
+    @RequestMapping("compareEmail.co")
+    @ResponseBody
+    public String emailCompare(@RequestParam("email_1") String email_1, @RequestParam("email_2") String email_2, Model model) { 
     
+		String email = email_1 + "@" + email_2;
+    	
+    	String userId = cService.compareEmail(email);
+    	
+    	String returnValue = "";
+		if(userId!=null) {
+			returnValue = userId;
+			System.out.println(returnValue);
+		} else {
+			returnValue = "fail";
+		}
+		return returnValue;
+	}	
+    
+ // 기업 아이디 찾기
+    @RequestMapping("compareEmail2.co")
+    @ResponseBody
+    public String emailCompare2(@RequestParam("busiemail_1") String busiemail_1, @RequestParam("busiemail_2") String busiemail_2, Model model) { 
+    
+		String busiemail = busiemail_1 + "@" + busiemail_2;
+    	
+    	String coId = cService.compareEmail2(busiemail);
+    	
+    	String returnValue = "";
+		if(coId!=null) {
+			returnValue = coId;
+			System.out.println(returnValue);
+		} else {
+			returnValue = "fail";
+		}
+		return returnValue;
+	}	
 
- // 비밀번호 찾기
-    @RequestMapping(value = "/sendMail/password", method = RequestMethod.POST)
-    public String sendMailPassword(HttpSession session, @RequestParam String id, @RequestParam String email, @RequestParam String captcha, RedirectAttributes ra) {
-        String captchaValue = (String) session.getAttribute("captcha");
-        if (captchaValue == null || !captchaValue.equals(captcha)) {
-            ra.addFlashAttribute("resultMsg", "자동 방지 코드가 일치하지 않습니다.");
-            return "redirect:/find/password";
-        }
- 
-        Member user = cService.findAccount(email);
+    // 비밀번호 찾기 : 임시 비밀번호 이메일로 보냄과 동시에 비번변경
+    @RequestMapping("mempwdFind.co")
+    public ModelAndView sendEmailAction (@RequestParam("email_1") String email_1, @RequestParam("email_2") String email_2, @RequestParam("memPwdEmail") String email, ModelMap model) throws Exception {
+        ModelAndView mav;
         
-        if (user != null) {
-            if (!user.getUserId().equals(id)) {
-                ra.addFlashAttribute("resultMsg", "입력하신 이메일의 회원정보와 가입된 아이디가 일치하지 않습니다.");
-                return "redirect:/find/password";
-            }
-            int ran = new Random().nextInt(100000) + 10000; // 10000 ~ 99999
-            String password = String.valueOf(ran);
-            CommonService.updateInfo(user.getUserId(), "password", password); // 해당 유저의 DB정보 변경
- 
-            String subject = "임시 비밀번호 발급 안내 입니다.";
-            StringBuilder sb = new StringBuilder();
-            sb.append("귀하의 임시 비밀번호는 " + password + " 입니다.");
-            mailService.send(subject, sb.toString(), "pbmin1993@gmail.com", email, null);
-            ra.addFlashAttribute("resultMsg", "귀하의 이메일 주소로 새로운 임시 비밀번호를 발송 하였습니다.");
-        } else {
-            ra.addFlashAttribute("resultMsg", "귀하의 이메일로 가입된 아이디가 존재하지 않습니다.");
+        String Email = email_1 + "@" + email_2;
+    	
+        System.out.println(Email+" "+email);
+        int pw=cService.getPw(Email, email);
+        System.out.println(pw);
+        
+        int ran = new Random().nextInt(100000) + 10000; // 10000 ~ 99999
+        String joinCode = String.valueOf(ran); // 임시비번
+        
+        String encPwd = bcryptPasswordEncoder.encode(joinCode);
+        
+        if(pw>0) {
+            EEmail.setContent("회원님의 비밀번호는 "+joinCode+" 입니다.");
+            EEmail.setReceiver(email);
+            EEmail.setSubject(Email+"님 비밀번호 찾기 메일입니다.");
+            emailSender.SendEmail(EEmail);
+            
+            cService.update_pw(encPwd);
+            
+            model.addAttribute("msg", "회원님의 메일로 임시 비밀번호를 발송 하였습니다.");
+            mav= new ModelAndView("redirect:/login.co");
+            return mav;
+        }else {
+        	System.out.println("실패");
+        	model.addAttribute("msg", "아이디 또는 이메일을 정확히 입력해 주세요.");
+            mav=new ModelAndView("redirect:/logout.co");
+            return mav;
         }
-        return "redirect:/find/password";
     }
-
+    
+ // join  뷰로 매핑
+    @RequestMapping(value = "/join", method = { RequestMethod.GET, RequestMethod.POST })
+    public String join(HttpServletResponse response, Model model) {
+        
+        OAuth2Operations oauthOperations = connectionFactory.getOAuthOperations();
+        String facebook_url = oauthOperations.buildAuthorizeUrl(GrantType.AUTHORIZATION_CODE, oAuth2Parameters);
+    
+        model.addAttribute("facebook_url", facebook_url);
+        System.out.println("/facebook" + facebook_url);
+ 
+        return "redirect:main.ho?menuNum=1&category=0";
+    }
+    
+   
+    
 }  
     
 	
--- src/main/java/com/kh/BucketStory/common/model/service/CommonServiceImpl.java
+++ src/main/java/com/kh/BucketStory/common/model/service/CommonServiceImpl.java
@@ -1,5 +1,7 @@
 package com.kh.BucketStory.common.model.service;
 
+import java.util.HashMap;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.kh.BucketStory.common.model.dao.CommonDAO;
 import com.kh.BucketStory.common.model.vo.Member;
 import com.kh.BucketStory.expert.model.vo.Company;
+import com.sun.javafx.collections.MappingChange.Map;
 
 @Service("cService")
 public class CommonServiceImpl implements CommonService {
@@ -41,10 +44,45 @@ public class CommonServiceImpl implements CommonService {
 	public int idDuplicateCheck(String id) {
 		return cDAO.idDuplicateCheck(sqlSession, id);
 	}
+	
+	@Override
+	public int idDuplicateCheck2(String id2) {
+		return cDAO.idDuplicateCheck2(sqlSession, id2);
+	}
 
+	@Override
+	public int nickNameCheck(String nick) {
+		return cDAO.nickNameCheck(sqlSession, nick);
+	}
 	@Override
 	public Member findAccount(String email) {
 		return cDAO.findAccount(sqlSession, email);
 	}
 
+	@Override
+	public String compareEmail(String email) {
+		return cDAO.compareEmail(sqlSession, email);
+	}
+	
+	@Override
+	public String compareEmail2(String busiemail) {
+		return cDAO.compareEmail2(sqlSession, busiemail);
+	}
+
+	@Override
+	public int getPw(String id, String email) {
+		return cDAO.getPw(sqlSession, id, email);
+	}
+
+	@Override
+	public int SearchPw(String id, String email, String joinCode) {
+		return cDAO.SearchPw(sqlSession, id, email, joinCode);
+	}
+
+	@Override
+	public void update_pw(String joinCode) {
+		
+	}
+
+	
 }
